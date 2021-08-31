95221beba32e69153ef948eb823c1902249bd167
Boba Fett <thomascupchurch@gmail.com>
2021-08-31 10:24:00 -0500

    hash updated passwords

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -54,12 +54,15 @@ router.post('/', (req, res) => {
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    
+    // pass in req.body instead to only update what's passed through
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
     })
-    .then(dvUserData => {
+    .then(dbUserData => {
         if (!dbUserData[0]) {
             res.status(404).json({ message: 'No user found with this is' });
             return;
