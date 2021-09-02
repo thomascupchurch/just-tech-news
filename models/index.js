fc48a0c381aad7a77999f36e5fb5d70066a118fe
Boba Fett <thomascupchurch@gmail.com>
2021-09-02 10:37:05 -0500

    post and user connection

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+const Vote = require('./Vote');
 const User = require('./User');
 const Post = require('./Post');
 
@@ -9,5 +10,18 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
 
 module.exports = { User, Post };
