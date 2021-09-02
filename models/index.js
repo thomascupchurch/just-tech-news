ae62041d30d32a8141183a1dd35b0b0d3146552c
Boba Fett <thomascupchurch@gmail.com>
2021-09-02 10:40:07 -0500

    connections between vote post and user

--- models/index.js
+++ models/index.js
@@ -23,5 +23,21 @@ Post.belongsToMany(User, {
     foreignKey: 'post_id'
 });
 
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-module.exports = { User, Post };
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Vote };
