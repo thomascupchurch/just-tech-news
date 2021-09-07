4f8c2ad910b54455f4bb7ea6c22ddbfd6d667ce8
Boba Fett <thomascupchurch@gmail.com>
2021-09-07 11:09:12 -0500

    add stylesheet

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 const router = require('./routes/api/user-routes');
+const path = require('path');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
