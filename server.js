2de25b294a8c3bde7ce5080c2a506ac5f1d3b638
Boba Fett <thomascupchurch@gmail.com>
2021-09-07 11:32:43 -0500

    add controllers folder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const router = require('./routes/api/user-routes');
+const router = require('./controllers/api/user-routes');
 const path = require('path');
 
 
