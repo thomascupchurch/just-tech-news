fd930e96cfabfb56a9b41166e55bdbbd7a5c3107
Boba Fett <thomascupchurch@gmail.com>
2021-09-07 14:58:49 -0500

    login working

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -23,6 +23,34 @@ async function signupFormHandler(event) {
             alert(response.statusText);
         }
     }
-}
+};
+
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  
+  
 
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
