cc599e6afbfad15a58d3ec162e633940821c6751
Boba Fett <thomascupchurch@gmail.com>
2021-09-08 17:19:23 -0500

    add third test

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -13,3 +13,15 @@ test('format_date() returns a date string', () => {
   });
 
   const {format_plural} = require('../utils/helpers');
+
+  test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+  
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
+  });
+
+  const {format_url} = require('../utils/helpers');
