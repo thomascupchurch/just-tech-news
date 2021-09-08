3027600b11c5cda266f87c2248e586054b8bc541
Boba Fett <thomascupchurch@gmail.com>
2021-09-08 17:15:14 -0500

    second test

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -5,3 +5,11 @@ test('format_date() returns a date string', () => {
   });
 
   const {format_date} = require('../utils/helpers');
+
+  test('format_plural() correctly pluralizes words', () => {
+      
+      expect(format_plural('Tiger', 2)).toBe('Tigers');
+      expect(format_plural('Lion', 1)).toBe('Lion');
+  });
+
+  const {format_plural} = require('../utils/helpers');
