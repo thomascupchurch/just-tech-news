c6c738c2a5ffe1d03b47690cde254ca25c5fbff1
Boba Fett <thomascupchurch@gmail.com>
2021-09-09 10:46:39 -0500

    add dashboard route

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,9 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 
+const dashboardRoutes = require('./dashboard-routes');
+
+router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
